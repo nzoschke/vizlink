674b9809ba7986f44db4e23bd111cd4896b59adc
Noah Zoschke <noah@mixable.net>
2024-03-30 13:57:46 -0700

    prettier

--- vizlink/src/main/java/net/mixable/vizlink/App.java
+++ vizlink/src/main/java/net/mixable/vizlink/App.java
@@ -4,10 +4,9 @@ package net.mixable.vizlink;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
 }
