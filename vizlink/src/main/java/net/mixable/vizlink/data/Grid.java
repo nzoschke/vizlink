1936f1614ee870d466a0356a79b437c27bc2e628
Noah Zoschke <noah@mixable.net>
2024-06-01 13:10:13 -0700

    Beat Grid RPC (#13)
    - RPC to print beat grid data

--- vizlink/src/main/java/net/mixable/vizlink/data/Grid.java
+++ vizlink/src/main/java/net/mixable/vizlink/data/Grid.java
@@ -1,7 +1,10 @@
 package net.mixable.vizlink.data;
 
+import io.kaitai.struct.ByteBufferKaitaiStream;
 import java.util.ArrayList;
 import org.deepsymmetry.beatlink.data.BeatGrid;
+import org.deepsymmetry.cratedigger.pdb.RekordboxAnlz;
+import org.deepsymmetry.cratedigger.pdb.RekordboxAnlz.SectionTags;
 
 // see also reflect-config.json
 public class Grid {
@@ -15,8 +18,28 @@ public class Grid {
     this.player = player;
 
     beats = new ArrayList<>();
-    for (int i = 1; i <= bg.beatCount; i++) {
-      beats.add(new GridBeat(i, bg.getTimeWithinTrack(i)));
+    for (int i = 0; i < bg.beatCount; i++) {
+      beats.add(new GridBeat(i + 1, bg.getTimeWithinTrack(i)));
+    }
+  }
+
+  public Grid(byte[] bs) {
+    RekordboxAnlz ext = new RekordboxAnlz(new ByteBufferKaitaiStream(bs));
+    for (RekordboxAnlz.TaggedSection ts : ext.sections()) {
+      if (ts.fourcc() == SectionTags.BEAT_GRID) {
+        init(ts);
+        return;
+      }
+    }
+  }
+
+  public void init(RekordboxAnlz.TaggedSection ts) {
+    RekordboxAnlz.BeatGridTag bg = new RekordboxAnlz.BeatGridTag(new ByteBufferKaitaiStream(ts._raw_body()));
+
+    beats = new ArrayList<>();
+    for (int i = 0; i < bg.numBeats(); i++) {
+      RekordboxAnlz.BeatGridBeat beat = bg.beats().get(i);
+      beats.add(new GridBeat(i + 1, beat.time()));
     }
   }
 }
