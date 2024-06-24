e83a187226d623754873a0a90747e9f7647a9f5e
Noah Zoschke <noah@mixable.net>
2024-06-24 15:29:31 -0700

    Add master and tempo to beat (#20)

--- vizlink/src/main/java/net/mixable/vizlink/data/Beat.java
+++ vizlink/src/main/java/net/mixable/vizlink/data/Beat.java
@@ -1,17 +1,23 @@
 package net.mixable.vizlink.data;
 
+import org.deepsymmetry.beatlink.CdjStatus;
+
 // see also reflect-config.json
 public class Beat {
 
   public Integer beat;
+  public Boolean master;
   public Boolean onAir;
   public Integer player;
+  public Double tempo;
 
   public Beat() {}
 
-  public Beat(Integer beat, Boolean onAir, Integer player) {
-    this.beat = beat;
-    this.onAir = onAir;
-    this.player = player;
+  public Beat(CdjStatus s) {
+    beat = s.getBeatNumber();
+    master = s.isTempoMaster();
+    onAir = s.isOnAir();
+    player = s.getDeviceNumber();
+    tempo = s.getEffectiveTempo();
   }
 }
